8f3aa4f64e7b4bfce5fb6827552a8e556ab6b6d6
Henrique Dias <hacdias@gmail.com>
2021-10-05 08:45:42 +0200

    detect unterminated contract

--- parser/fintracts.ml
+++ parser/fintracts.ml
@@ -3,10 +3,16 @@ open Types
 module JSON = Yojson.Safe
 
 let main =
+  let got_contract = ref false in
   try
     let lexbuf = Lexing.from_channel stdin in
       let result = Parser.main Lexer.token lexbuf in
         let yo = contract_to_yojson result in
+          got_contract := true;
           print_endline (JSON.pretty_to_string yo);
   with Lexer.Eof ->
-    exit 0
+    if got_contract.contents == true then
+      exit 0
+    else
+      print_endline "Fatal: contract unterminated.";
+      exit 1
