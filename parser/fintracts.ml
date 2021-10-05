45f320ac04dd27cbdb8d4620230cc5ff058c509d
Henrique Dias <hacdias@gmail.com>
2021-10-05 15:54:50 +0200

    start contract validation

--- parser/fintracts.ml
+++ parser/fintracts.ml
@@ -1,29 +1,7 @@
-open Types
+open Lib
 open Core
 
-exception ImcompleteContract
 exception InvalidNumberInputFiles
-
-module JSON = Yojson.Safe
-
-let parse_contract lexbuf =
-  try
-    Parser.main Lexer.token lexbuf
-  with Lexer.Eof ->
-    raise ImcompleteContract
-
-let parse_from_channel ch =
-  let lexbuf = Lexing.from_channel ch in
-    parse_contract lexbuf
-
-let parse_from_file path =
-  let file = In_channel.read_all path in
-    let lexbuf = Lexing.from_string file in
-      parse_contract lexbuf
-
-let pretty_contract contract =
-  let json = contract_to_yojson contract in
-    JSON.pretty_to_string json
 
 let usage_msg = "fintracts [-json] <input file>"
 
@@ -43,5 +21,6 @@ let () =
   let contract = match input_file.contents with
     | "" -> parse_from_channel In_channel.stdin
     | filename -> parse_from_file filename in
+    validate contract;
     if print_json.contents then
       print_endline (pretty_contract contract)
