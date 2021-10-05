37850c2c6f210575cf333d6ea522484d1febd907
Henrique Dias <hacdias@gmail.com>
2021-10-05 13:59:04 +0200

    update fintracts

--- parser/fintracts.ml
+++ parser/fintracts.ml
@@ -1,18 +1,47 @@
 open Types
+open Core
+
+exception ImcompleteContract
+exception InvalidNumberInputFiles
 
 module JSON = Yojson.Safe
 
-let main =
-  let got_contract = ref false in
+let parse_contract lexbuf =
   try
-    let lexbuf = Lexing.from_channel stdin in
-      let result = Parser.main Lexer.token lexbuf in
-        let yo = contract_to_yojson result in
-          got_contract := true;
-          print_endline (JSON.pretty_to_string yo);
+    Parser.main Lexer.token lexbuf
   with Lexer.Eof ->
-    if got_contract.contents == true then
-      exit 0
-    else
-      print_endline "Fatal: contract unterminated.";
-      exit 1
+    raise ImcompleteContract
+
+let parse_from_channel ch =
+  let lexbuf = Lexing.from_channel ch in
+    parse_contract lexbuf
+
+let parse_from_file path =
+  let file = In_channel.read_all path in
+    let lexbuf = Lexing.from_string file in
+      parse_contract lexbuf
+
+let pretty_contract contract =
+  let json = contract_to_yojson contract in
+    JSON.pretty_to_string json
+
+let usage_msg = "fintracts [-json] <input file>"
+
+let print_json = ref false
+let input_file = ref ""
+
+let arg_parser filename =
+  match input_file.contents with
+    | "" -> input_file := filename
+    | _ -> raise InvalidNumberInputFiles
+
+let speclist =
+  [("-json", Arg.Set print_json, "Print parsed JSON.")]
+
+let () =
+  Arg.parse speclist arg_parser usage_msg;
+  let contract = match input_file.contents with
+    | "" -> parse_from_channel In_channel.stdin
+    | filename -> parse_from_file filename in
+    if print_json.contents then
+      print_endline (pretty_contract contract)
